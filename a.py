de42204aa7db0469ade9a964a081544597e78cb3
Jon <Jonsivert.pettersen@gmail.com>
2022-10-18 11:13:10 +0200

    HotFix
    change in code

--- a.py
+++ a.py
@@ -1,9 +1,10 @@
 print("Hello World!")
 
-text = None
-while type(text) == str:
+text = 0
+while type(text) != str:
     try:
         text = str(input("Write a string: "))
     except ValueError:
         pass
-    print(text)
+print(text)
+    
