d750d2b1fefa9282c11f230bdd61c2857da04bb8
Jon <Jonsivert.pettersen@gmail.com>
2022-11-07 13:37:28 +0100

    Update Person_kassen.py

--- Oving10/Person_kassen.py
+++ Oving10/Person_kassen.py
@@ -1,1 +1,9 @@
+class Person:
+    def __init__(self, id, navn, tlf, epost):
+        self.id = id
+        self.navn = navn
+        self.tlf = tlf
+        self.epost = epost
 
+    def __str__(self):
+        return f"ID: {self.id}, Navn: {self.navn},tlf: {self.tlf}, epost: {self.epost}"
