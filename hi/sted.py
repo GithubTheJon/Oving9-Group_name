568cfff6d6076eb7819716b7317c708eb8df1a9b
Jon <Jonsivert.pettersen@gmail.com>
2022-11-16 22:12:56 +0100

    Update sted.py

--- hi/sted.py
+++ hi/sted.py
@@ -1,33 +1,52 @@
-# deloppgaver; g, h, i
+# Deloppgaver; g, h, i
 import os
 
 
-#opg g
+# opg g
 class Sted:
-    #opg g
-    def __init__(self, id, navn, addresse, postnummer, poststed):
+    # opg g
+    def __init__(self, id, navn, addresse = 0, postnummer = 0, poststed = 0):
         self.id = id
         self.navn = navn
         self.addresse = addresse
         self.postnummer = postnummer
         self.poststed = poststed
 
-    #opg g
+    # opg g
     def __str__(self):
         return f"Sted med id {self.id}, navn {self.navn}, addresse {self.addresse}, postnummer {self.postnummer}, poststed {self.poststed}"
 
 
-#opg h
-def lag_sted():
-    id = int(input("Skriv inn id til stedet: "))
-    navn = input("Skriv inn hva stedet heter: ")
-    addresse = input("Skriv inn addressen til stedet: ")
-    postnummer = int(input("Skriv inn postnummered til stedet: "))
-    poststed = input("Skriv inn poststedet til stedet: ")
-    return Sted(id, navn, addresse, postnummer, poststed)
+# opg h
+def lag_sted(sted_liste):
+    valg = -1
+    print("Et sted er nødvendig")
+    print("1: lag nytt sted: ")
+    print("2: velg et sted: ")
+    valg = int(input("Velg: "))
+    if valg == 1:
+        start_gyldig = False
+        while not start_gyldig:
+            try:
+                ids = int(input("Skriv inn id til stedet: "))
+                navn = input("Skriv inn hva stedet heter: ")
+            except ValueError:
+                print("Ugyldig!")
+            else:
+                start_gyldig = True
+        return Sted(ids, navn)
+
+    elif valg == 2:
+        i = 0
+        for sted in sted_liste:
+            print(f"index {i}; {sted}")
+            i += 1
+        stedet = int(input("Velg en index: "))
+        print(f"Valgte: {sted_liste[stedet]}")
+        return sted_liste[stedet]
 
 
-#opg i
+# opg i
 def les_sted_fra_fil(fil):
     if not os.path.exists(fil):
         print("Denne filen eksisterer ikke!")
@@ -42,7 +61,7 @@ def les_sted_fra_fil(fil):
     return leste_steder
 
 
-#opg i
+# opg i
 def skriv_sted_til_fil(fil, sted_liste):
     if os.path.exists(fil):
         print("Denne filen eksisterer allerede, overskriver")
